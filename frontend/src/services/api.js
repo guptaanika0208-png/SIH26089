f4c377ba54878f15dd1d6356cb4f57f91bcf1557
Anika Gupta <guptaanika0208@gmail.com>
2026-09-12 12:35:41 +0530

    Point frontend to deployed backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ghargo-backend.onrender.com/api',
 });
 
-// attach token automatically if logged in
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
